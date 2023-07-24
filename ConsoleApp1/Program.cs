2fa77df664072feda019394638173b476dc86d58
Alexandr Uvaev <alex211275@yandex.ru>
2023-07-24 21:18:09 +0300

    Сформирована программа анкета из модуля 3.3.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,10 +4,17 @@
     {
         static void Main(string[] args)
         {
-            string MyName;
-            MyName = "Alexandr";
-            Console.WriteLine(MyName);
-            Console.ReadKey();
+            string MyName = "Alexandr";
+            int MyAge = 47;
+            bool IsZoo = false;
+            int ValSwoe = 43;
+
+            Console.WriteLine("Мое  имя " + MyName);
+            Console.WriteLine("Мне " + MyAge + " лет");
+            Console.WriteLine("Имею ли я животное? " + IsZoo);
+            Console.WriteLine("Мой  размер обуви - " + ValSwoe);
+            Console.ReadLine();
         }
     }
 }
+
