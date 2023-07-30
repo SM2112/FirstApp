d1cdf15211ac4d03dacfe5aa7beb7f55ee80ec32
Alexandr Uvaev <alex211275@yandex.ru>
2023-07-30 10:55:06 +0300

    Подготовле файл с итоговым заданием по модулю 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,20 +1,42 @@
-﻿namespace ConsoleApp1
+﻿using System.Collections.Specialized;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string MyName = "Alexandr";
-            int MyAge = 47;
-            bool IsZoo = false;
-            int ValSwoe = 43;
+            // КОД ИТОГОВОЙ ПРРОГРАМММЫ ПО МОДУЛЮ 3
+            // ЗА ОСНОВУ ВЫВОДА ВЗЯТ ПОСЛЕДНИЙ СКРИН С РАЗДЕЛА 3.7
 
-            Console.WriteLine("Мое  имя " + MyName);
-            Console.WriteLine("Мне " + MyAge + " лет");
-            Console.WriteLine("Имею ли я животное? " + IsZoo);
-            Console.WriteLine("Мой  размер обуви - " + ValSwoe);
-            Console.ReadLine();
+            //раздел объявления переменных
+            byte age;
+            bool iscorrect;  // переменная для понимания корректно ли произошло преобразование возрасста
+            string name, birthday;
+
+            // раздел сбора и вывода информации о пользователе
+            Console.WriteLine("Привет, пользорватель!");
+            Console.WriteLine("Я  компютер");
+            Console.WriteLine("Я собираю анкету о тебе. Начнем!");
+            
+            Console.Write("Введите свое имя: ");
+            name = Console.ReadLine();
+            
+            Console.Write("Введите свой возраст: ");
+            iscorrect = byte.TryParse(Console.ReadLine(), out age);
+            
+            Console.WriteLine($"Проверяем! Ваше имя {name} и Ваш возраст {age} лет!");
+            Console.WriteLine($"Проверка корректности ввода возраста показала {iscorrect}");
+
+            Console.Write("Ваша дата рождения: ");
+            birthday = Console.ReadLine();
+
+            Console.WriteLine("{0}, Ваша дата рождения - {1}", name, birthday);
+            Console.ReadKey();
+
         }
+       
     }
 }
 
